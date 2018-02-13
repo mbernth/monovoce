6265124d664b4576881c16f69f552e8e882300bf
Michael Bernth <michael@monovoce.dk>
2018-02-13 21:19:09 +0100

    Navigation collapsed

--- monovoce/js/responsive-menu.js
+++ monovoce/js/responsive-menu.js
@@ -15,48 +15,14 @@ jQuery(function( $ ){
 		}
 
 	});
+
+	$("header .genesis-nav-menu, .nav-primary .genesis-nav-menu").addClass("responsive-menu").before('<div class="responsive-menu-icon"><div class="bar"></div></div>');
 	
-	/*
-	$("header .genesis-nav-menu, .nav-primary .genesis-nav-menu, .nav-secondary .genesis-nav-menu").addClass("responsive-menu").before('<div class="responsive-menu-icon"><svg class="icon-list2"><use xlink:href="#icon-list2"></use></svg></div>');
-	
-	
-	$(".responsive-menu-icon").click(function(){
-		$(this).next("header .genesis-nav-menu, .nav-primary .genesis-nav-menu, .nav-secondary .genesis-nav-menu").slideToggle();
-		$( '.responsive-menu-icon' ).toggleClass("icon-open");
-	});
-	*/
-	
-	
-	$("header .genesis-nav-menu, .nav-primary .genesis-nav-menu").addClass("responsive-menu").before('<div class="responsive-menu-icon"><svg class="icon-list2"><use xlink:href="#icon-list2"></use></svg></div>');
-	
-	$(".nav-secondary .genesis-nav-menu").addClass("responsive-menu-two").before('<div class="responsive-menu-icon-two"><svg class="icon-list2"><use xlink:href="#icon-list2"></use></svg></div>');
-	
-	$(".responsive-menu-icon").click(function(){
+	$('.responsive-menu-icon').on('click', function() {
 		$(this).next("header .genesis-nav-menu, .nav-primary .genesis-nav-menu").slideToggle();
 		$( '.responsive-menu-icon' ).toggleClass("icon-open");
+		$('.bar').toggleClass('animate');
+		$('.site-header').toggleClass('main-menu-open');
 	});
-	
-	$(".responsive-menu-icon-two").click(function(){
-		$(this).next(".nav-secondary .genesis-nav-menu").slideToggle();
-		$( '.responsive-menu-icon-two' ).toggleClass("icon-open-two");
-	});
-	
-	
-	/*
-	$(window).resize(function(){
-		if(window.innerWidth > 768) {
-			$("header .genesis-nav-menu, .nav-primary .genesis-nav-menu, .nav-secondary .genesis-nav-menu, nav .sub-menu").removeAttr("style");
-			$(".responsive-menu > .menu-item").removeClass("menu-open");
-		}
-	});
-	
-	$(".responsive-menu > .menu-item").click(function(event){
-		if (event.target !== this)
-		return;
-			$(this).find(".sub-menu:first").slideToggle(function() {
-			$(this).parent().toggleClass("menu-open");
-		});
-	});
-	*/
 
 });
