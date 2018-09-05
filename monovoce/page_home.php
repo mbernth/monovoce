528264abdcb80589c3205f67e27dc29ffcc5bed2
mbernth <michael@monovoce.dk>
2018-09-05 18:11:32 +0200

    Typography, hero and menu

--- monovoce/page_home.php
+++ monovoce/page_home.php
@@ -32,70 +32,30 @@ remove_action( 'genesis_before_loop', 'genesis_do_breadcrumbs' );
 //* Force full width content
 add_filter( 'genesis_pre_get_option_site_layout', '__genesis_return_full_width_content' );
 
-//* Top Slider
+//* Hero element
 // =====================================================================================================================
 
-add_action( 'wp_enqueue_scripts', 'enqueue_scripts_frontpage_image' );
-function enqueue_scripts_frontpage_image() {
-
-	wp_enqueue_script( 'frontpage-owl-min', get_stylesheet_directory_uri() . '/js/owl.carousel.min.js', array( 'jquery' ), '1.0.0', true );
-	wp_enqueue_script( 'frontpage-owl', get_stylesheet_directory_uri() . '/js/owl.frontslider.js', array( 'jquery' ), '1.0.0', true );
-	
-}
-
 // check if the flexible content field has rows of data
-add_action( 'genesis_after_header', 'mono_frontpage_slider' );
-function mono_frontpage_slider() {
-	$loopCount = 0;
-	$arrowlink = get_field( 'arrow_link_url' );
-	
-	if( have_rows('slider') ):
-		
-		echo '<div id="owl-slider" class="owl-carousel owl-theme">';
-		
-		// loop through the rows of data
-    	while ( have_rows('slider') ) : the_row();
+add_action( 'genesis_after_header', 'mono_hero_element' );
+function mono_hero_element() {
+	$maintext = get_field( 'main_text' );
 
-        	if( get_row_layout() == 'slide' ):
-				
-				echo '<div class="item">';
-				echo '<div class="picture-section" style="background-image:url( ';
-						the_sub_field('image');
-				echo ');"><div class="height-section">';
-				
-				echo '<div class="slide_content">';
-						the_sub_field('text');
-						the_sub_field('content_text');
-				echo '</div>';
-								
-				echo '</div></div>';
-				echo '</div>';
-				$loopCount ++;
-			
-			endif;
-
-    	endwhile;
-		
+	if ($maintext):
+		echo '<div class="gridcontainer non hero">';
+			echo '<div class="wrap">';
+				echo '<h1 class="hero-text">'.$maintext.'<br>';
+					if ( have_rows( 'text_animation' ) ) :
+						while ( have_rows( 'text_animation' ) ) : the_row();
+						$word = get_sub_field( 'word' );
+							echo '<span>'.$word.'</span>';
+						endwhile;
+					else :
+					// no rows found
+					endif;
+				echo '</h1>';
+			echo '</div>';
 		echo '</div>';
-
-		if ( have_rows( 'text_animation' ) ) :
-			echo '<div class="statements"><h1>We make future<br>friendly ';
-			echo '<div class="flip">';
-			while ( have_rows( 'text_animation' ) ) : the_row();
-				echo '<div><div>';
-					the_sub_field( 'word' );
-				echo '</div></div>';
-			endwhile;
-			echo '</h1></div></div>';
-
-			else :
-			// no rows found
-		endif;
-	
-	else :
-
-    // no layouts found
-
+	else:
 	endif;
 	
 }
